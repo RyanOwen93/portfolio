0326c86d09a3b0161bacdc775f6620b6defddbb1
Ryan Owen <RyanOwen@ryans-mac.home>
2018-04-15 17:58:57 +0100

    Changed format of portfolio page

--- script.js
+++ script.js
@@ -85,7 +85,7 @@ var modal = document.getElementById('skateModal');
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('skateWebsite');
 var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var captionText = document.getElementById("skatecaption");
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -93,7 +93,7 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("skateclose")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
@@ -108,7 +108,7 @@ var modal = document.getElementById('sassModal');
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img = document.getElementById('sassWebsite');
 var modalImg = document.getElementById("img02");
-var captionText = document.getElementById("caption");
+var captionText = document.getElementById("sasscaption");
 img.onclick = function(){
     modal.style.display = "block";
     modalImg.src = this.src;
@@ -116,7 +116,7 @@ img.onclick = function(){
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("sassclose")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
